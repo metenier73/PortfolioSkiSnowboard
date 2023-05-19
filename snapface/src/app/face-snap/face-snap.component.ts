fcf2d4ba1cf6cfab69b6bc3b4e00ef99a66ec80b
metenier <meteniermyriam@yahoo.fr>
2023-05-19 13:00:16 +0200

    modified:   snapface/src/app/app.component.html 	modified:   snapface/src/app/app.module.ts 	new file:   snapface/src/app/face-snap/face-snap.component.html 	new file:   snapface/src/app/face-snap/face-snap.component.scss 	new file:   snapface/src/app/face-snap/face-snap.component.spec.ts 	new file:   snapface/src/app/face-snap/face-snap.component.ts 	modified:   snapface/src/index.html 	modified:   snapface/src/styles.scss

---
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  ngOnInit(){
+     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+     this.title = 'Archibald';
+     this.description = 'Mon meilleur ami depuis tout petit !';
+     this.createDate = new Date();
+     this.snaps = 6;
+  }
+
+}
