39e2f1055794e9987688996c287861af5f1dae13
metenier <meteniermyriam@yahoo.fr>
2023-05-24 19:30:12 +0200

    24-05

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit() {
     this.mySnap = new FaceSnap(
@@ -17,5 +19,24 @@ export class AppComponent implements OnInit {
       new Date(),
       0
     );
+
+    this.myOtherSnap = new FaceSnap(
+      'Three Rock Mountain',
+      'Un endroit magnifique pour les randonnées.',
+      'https://pixabay.com/fr/photos/cheval-blanc-hiver-neige-d%c3%a9cembre-3010129.jpg/',
+      new Date(),
+      0
+    );
+
+    this.myLastSnap = new FaceSnap(
+      'Un bon repas',
+      'Mmmh qe c\'est bon !',
+      'https://pixabay.com/fr/photos/brown-papillon-blanc-papillons-209106/',
+      new Date(),
+      0
+    );
+
+
+
   }
 }
