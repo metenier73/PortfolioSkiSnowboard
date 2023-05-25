11cb7a08ee02150ce86e88543c8523732ae44101
metenier <meteniermyriam@yahoo.fr>
2023-05-25 09:48:54 +0200

    25B

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -43,14 +43,16 @@ export class AppComponent implements OnInit {
       description: 'Mon meilleur ami depuis tout petit !',
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 0,
+      location: 'Paris'
     };
     this.myOtherSnap = {
       title: 'Three Rock Mountain',
       description: 'Un endroit magnifique pour les randonnées.',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 0,
+      location: 'la montagne'
     };
     this.myLastSnap = {
       title: 'Un bon repas',
@@ -58,6 +60,7 @@ export class AppComponent implements OnInit {
       imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
       createdDate: new Date(),
       snaps: 0
+      
     };
 >>>>>>> d13ecfa (25-05)
 
