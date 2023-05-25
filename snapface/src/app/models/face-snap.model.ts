d13ecfafdb333613dcfb17a4b8632176f9d7789a
metenier <meteniermyriam@yahoo.fr>
2023-05-25 09:35:17 +0200

    25-05

--- snapface/src/app/models/face-snap.model.ts
+++ snapface/src/app/models/face-snap.model.ts
@@ -1,16 +1,16 @@
 export class FaceSnap {
-    title: string;
-    description: string;
-    createdDate: Date;
-    snaps: number;
-    imageUrl: string;
-    
-    constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number) {
-      this.title = title;
-      this.description = description;
-      this.imageUrl = imageUrl;
-      this.createdDate = createdDate;
-      this.snaps = snaps;
-    }
+    title!: string;
+    description!: string;
+    createdDate!: Date;
+    snaps!: number;
+    imageUrl!: string;
+    location?: string;
+
+    // constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number) {
+    //   this.title = title;
+    //   this.description = description;
+    //   this.imageUrl = imageUrl;
+    //   this.createdDate = createdDate;
+    //   this.snaps = snaps;
+    // }
   }
-  
