56093780364e044267e30596677aedc20f2255b8
metenier <meteniermyriam@yahoo.fr>
2023-05-25 13:04:57 +0200

    new file:   .angulardoc.json 	new file:   .vscode/settings.json 	modified:   snapface/src/app/app.component.html 	modified:   snapface/src/app/app.component.ts

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -7,39 +7,7 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-<<<<<<< HEAD
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
 
-  ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
-
-    this.myOtherSnap = new FaceSnap(
-      'Three Rock Mountain',
-      'Un endroit magnifique pour les randonnées.',
-      'https://pixabay.com/fr/photos/cheval-blanc-hiver-neige-d%c3%a9cembre-3010129.jpg/',
-      new Date(),
-      0
-    );
-
-    this.myLastSnap = new FaceSnap(
-      'Un bon repas',
-      'Mmmh qe c\'est bon !',
-      'https://pixabay.com/fr/photos/brown-papillon-blanc-papillons-209106/',
-      new Date(),
-      0
-    );
-
-
-
-=======
   faceSnaps!: FaceSnap[];
 
   ngOnInit() {
@@ -68,6 +36,6 @@ export class AppComponent implements OnInit {
         snaps: 0
       }
     ];
->>>>>>> master
+
   }
 }
