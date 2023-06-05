c3a93756c6590ec1745fb57373de729a4dde5ccf
metenier <meteniermyriam@yahoo.fr>
2023-06-05 14:27:14 +0200

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/face-snap-list/face-snap-list.component.html 	new file:   src/app/face-snap-list/face-snap-list.component.scss 	new file:   src/app/face-snap-list/face-snap-list.component.spec.ts 	new file:   src/app/face-snap-list/face-snap-list.component.ts 	new file:   src/app/header/header.component.html 	new file:   src/app/header/header.component.scss 	new file:   src/app/header/header.component.spec.ts 	new file:   src/app/header/header.component.ts 	modified:   src/styles.scss

--- snapface/src/app/app.module.ts
+++ snapface/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
 import { AppRoutingModule } from './app-routing.module';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent
+    FaceSnapComponent,
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
